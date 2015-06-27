fc46816d83bf7dc626f065080935ae7355a39e03
Andi Talisman <andi.edge@gmail.com>
2015-06-27 20:35:31 +0700

    upload.php

---
+++ upload.php
@@ -0,0 +1,42 @@
+<?php
+include "connect.php";
+$target_dir = "image/";
+$target_file = $target_dir . basename($_FILES["gambar"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    // Simpan ke database
+    $sql = "insert into simpan (nama, fase, kategori, kavling, gambar, keterangan) values ('".$_POST['nama']."', '".$_POST['fase']."', '".$_POST['kategori']."',  '".$_POST['kav']."', '$target_file', '".$_POST['keterangan']."')";
+    mysql_query($sql);
+    $check = getimagesize($_FILES["gambar"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["gambar"]["size"] > 5000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file)) {
+        echo "<script>alert('The file ". basename( $_FILES["gambar"]["name"]). " has been uploaded.');history.go(-1);</script>";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
